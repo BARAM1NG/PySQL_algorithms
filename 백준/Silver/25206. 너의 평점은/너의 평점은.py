2371b61d4f0c1eb38987dc9b8eb00f65a87343fc
ahram_bae <122276734+BARAM1NG@users.noreply.github.com>
2024-01-01 22:18:01 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,19 @@
+rating = {
+    'A+' : 4.5, 'A0' : 4.0, 'B+' : 3.5, 'B0' : 3.0, 'C+' : 2.5, 'C0' : 2.0, 'D+' : 1.5,
+    'D0' : 1.0, 'F' : 0.0
+    }
+
+# 학점 총점
+A = 0
+
+# 학점 * 과목평점 총점
+B = 0
+
+for i in range(20):
+    subject, score, grade = input().split()
+    if grade == 'P':
+        continue
+    A += float(score)
+    B += float(score) * rating[grade]
+    
+print(B/A)
